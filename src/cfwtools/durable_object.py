0f50889c19ba2b6a2174c960f8820ea5c84b41b4
Vladislav A. Proskurov <rilshok@pm.me>
2026-06-14 15:52:05 +0400

    feat: do

---
+++ src/cfwtools/durable_object.py
@@ -0,0 +1,24 @@
+from functools import cached_property
+
+from workers import DurableObject as _DurableObject
+from cfwtools.sql import Sql
+
+if TYPE_CHECKING:
+    from js import DurableObjectState, Env
+
+
+class DurableObject(_DurableObject):
+    def __init__(self, ctx: "DurableObjectState", env: "Env") -> None:
+        super().__init__(ctx, env)
+        self.__post_init__()
+
+    @cached_property
+    def name(self) -> str:
+        return self.ctx.id.name
+
+    @cached_property
+    def sql(self) -> Sql:
+        return Sql(self.ctx.storage.sql)
+
+    def __post_init__(self) -> None:
+        pass
